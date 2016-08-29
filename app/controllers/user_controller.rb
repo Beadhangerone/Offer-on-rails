721fe8f5acf6a81e9b7b5984740c10f14627bf32
Beadhangerone <denm.2206@gmail.com>
2016-08-29 10:08:18 +0300

    saving values on error at reg & log

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,31 +3,35 @@ class UserController < ApplicationController
 
 
   def create
-    @reg_username = params[:reg_username]
+    $reg_username = params[:reg_username]
     @reg_pass = params[:reg_pass]
     @pass_check = params[:pass_check]
     $error = validation_for("registration").join(" | ")
     if $error == ""
       $client = User.new 
-      $client.username = @reg_username
+      $client.username = $reg_username
       $client.pass = @reg_pass
       $client.save
       $success = "Регистрация прошла успешно"
+      $reg_username = nil
+      @reg_pass = nil
       redirect_to profile_path
     else
-      redirect_to "/registration"
+      redirect_to "/"
     end
   end
 
   def login
-    @username = params[:username]
+    $username = params[:username]
     @pass = params[:pass]
     $error = validation_for("login").join(" | ")
-    if $error != ""
-      redirect_to "/"
-    else
+    if $error == ""
       $success = "Вход прошел успешно"
-      redirect_to profile_path  
+      $username = nil
+      @pass = nil
+      redirect_to profile_path
+    else
+      redirect_to "/"
     end
   end
 
@@ -50,12 +54,14 @@ class UserController < ApplicationController
 
   def validation_for(type)
     errors=[]
+
     if type == "registration"
-      if User.where(username: "#{@reg_username}").take != nil
+
+      if User.where(username: "#{$reg_username}").take != nil
         errors << "Логин занят"
-      elsif @reg_username.length == 0
+      elsif $reg_username.length == 0
         errors << "Введите логин"
-      elsif @reg_username.length < 3 && @reg_username.length > 0
+      elsif $reg_username.length < 3 && $reg_username.length > 0
         errors << "Логин слишком короткий(минимум 3 символа)"
       end
 
@@ -66,9 +72,11 @@ class UserController < ApplicationController
       end
 
       errors << "Пароли не совпадают" if @reg_pass.to_s != @pass_check.to_s
-      return errors          
+      return errors 
+
     elsif type == "login"
-      if @username.length == 0
+      
+      if $username.length == 0
         errors << "Введите логин"
       end
 
@@ -76,8 +84,8 @@ class UserController < ApplicationController
         errors << "Введите пароль"
       end
 
-      if @username != "" && @pass != ""
-        $client = User.where(username: "#{@username}").take
+      if $username != "" && @pass != ""
+        $client = User.where(username: "#{$username}").take
         if $client == nil or $client.pass != @pass
           $client = nil
           errors << "Неверный логин или пароль"
