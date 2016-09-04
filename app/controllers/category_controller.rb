0154852d20ef26e73b8a3f49a853a47a45db71ac
Beadhangerone <denm.2206@gmail.com>
2016-09-04 13:04:11 +0300

    showing ads by category on the page

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,7 @@
 class CategoryController < ApplicationController
   def show
     $category = Category.where(url: "#{params[:id]}").take
+    $ads = Ad.where(category: "#{$category.category}").order(created_at: :desc)
     @title = $category.category
   end
 end
