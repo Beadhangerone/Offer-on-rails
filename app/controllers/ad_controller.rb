8a779753a7550ea402f9ddffdad72c082e749e46
Beadhangerone <denm.2206@gmail.com>
2016-09-13 20:04:59 +0300

    saving values on error

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -12,29 +12,32 @@ class AdController < ApplicationController
   end
 
   def create
-    @category = params[:category]
-    @bill_type = params[:bill_type]
-    @ad_header = params[:ad_header]
-    @ad_text = params[:ad_text]
-
-    
+    $category = params[:category]
+    $bill_type = params[:bill_type]
+    $ad_header = params[:ad_header]
+    $ad_text = params[:ad_text]
+  
     $error = validation_for("ad/new").join " | "
 
-    if $error != ""
-      redirect_to "/ad"
-    else
+    if $error == ""
       @ad=Ad.new 
         @ad.author = $client.username
-        @ad.category = @category
-        @ad.bill_type = @bill_type
-        @ad.ad_header = @ad_header
-        @ad.ad_text = @ad_text
+        @ad.category = $category
+        @ad.bill_type = $bill_type
+        @ad.ad_header = $ad_header
+        @ad.ad_text = $ad_text
       @ad.save
 
-      $client.total_ads +=1
+      $client.total_ads += 1
       $client.save
-      $success="Объявление подано!"
+      $success = "Объявление подано!"
       redirect_to @ad
+      $category = nil
+      $bill_type = nil
+      $ad_header = nil
+      $ad_text = nil
+    else
+      redirect_to new_ad_path
     end   
   end
 
@@ -48,15 +51,15 @@ class AdController < ApplicationController
   end
 
   def update
-    @category = $ad.category
-    @bill_type = params[:bill_type]
-    @ad_header = params[:ad_header]
-    @ad_text = params[:ad_text]
+    $category = $ad.category
+    $bill_type = params[:bill_type]
+    $ad_header = params[:ad_header]
+    $ad_text = params[:ad_text]
     $error = validation_for("ad/new").join(" | ")
     if $error == ""
-      $ad.bill_type = @bill_type
-      $ad.ad_header = @ad_header
-      $ad.ad_text = @ad_text
+      $ad.bill_type = $bill_type
+      $ad.ad_header = $ad_header
+      $ad.ad_text = $ad_text
       $ad.save
       $success="Объявление отредактировано!"
       redirect_to $ad
@@ -84,10 +87,10 @@ class AdController < ApplicationController
   def validation_for(type)
     errors=[]
     if type == "ad/new"
-      errors << 'Выберите категорию' if @category.to_s.length==0
-      errors << 'Выберите тип предложения' if @bill_type.to_s.length==0
-      errors << 'Введите заголовок объявления' if @ad_header.to_s.length==0
-      errors << 'Введите текст объявления' if @ad_text.to_s.length==0
+      errors << 'Выберите категорию' if $category.to_s.length == 0
+      errors << 'Выберите тип предложения' if $bill_type.to_s.length == 0
+      errors << 'Введите заголовок объявления' if $ad_header.to_s.length == 0
+      errors << 'Введите текст объявления' if $ad_text.to_s.length == 0
     end
     return errors
   end
